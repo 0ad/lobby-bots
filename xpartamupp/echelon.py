0ad/lobby-bots

commit 219ce9f2e6973ba1476ccb691d2f3519bcc48c5a

Replace SleekXMPP with Slixmpp
SleekXMPP has been declared end-of-life for years and hasn't seen any
activity since. It also support only Python up to version 3.7. There is
a friendly fork called Slixmpp however, which sees continuous
development and releases and supports newer Python versions. To be able
to support newer Python versions with the lobby bots and to avoid using
abandoned libraries as dependencies, this replaces SleekXMPP with
Slixmpp.

xpartamupp/echelon.py
@@ -19,18 +19,19 @@
 """0ad XMPP-bot responsible for managing game ratings."""
 
 import argparse
+import asyncio
 import difflib
 import logging
 import sys
 
 from collections import deque
 
-import sleekxmpp
-
-from sleekxmpp.stanza import Iq
-from sleekxmpp.xmlstream.handler import Callback
-from sleekxmpp.xmlstream.matcher import StanzaPath
-from sleekxmpp.xmlstream.stanzabase import register_stanza_plugin
+from slixmpp import ClientXMPP
+from slixmpp.jid import JID
+from slixmpp.stanza import Iq
+from slixmpp.xmlstream.handler import Callback
+from slixmpp.xmlstream.matcher import StanzaPath
+from slixmpp.xmlstream.stanzabase import register_stanza_plugin
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -62,7 +63,7 @@ class Leaderboard:
         create him first, if he doesn't exist yet.
 
         Arguments:
-            jid (sleekxmpp.jid.JID): JID of the player to get
+            jid (JID): JID of the player to get
 
         Returns:
             Player instance representing the player specified by the
@@ -83,8 +84,7 @@ class Leaderboard:
         """Get the leaderboard profile for the specified player.
 
         Arguments:
-            jid (sleekxmpp.jid.JID): JID of the player to retrieve the
-                profile for
+            jid (JID): JID of the player to retrieve the profile for
 
         Returns:
             dict with statistics about the requested player or None if
@@ -175,7 +175,7 @@ class Leaderboard:
 
         player_infos = []
         for player in players:
-            player_jid = sleekxmpp.jid.JID(player.jid)
+            player_jid = JID(player.jid)
             player_info = PlayerInfo(player=player)
             for report_name in stats:
                 setattr(player_info, report_name, game_report[report_name][player_jid])
@@ -250,8 +250,8 @@ class Leaderboard:
             result_qualitative = 'drew'
         else:
             result_qualitative = 'lost'
-        name1 = sleekxmpp.jid.JID(player1.jid).local
-        name2 = sleekxmpp.jid.JID(player2.jid).local
+        name1 = JID(player1.jid).local
+        name2 = JID(player2.jid).local
         self.rating_messages.append("A rated game has ended. %s %s against %s. Rating "
                                     "Adjustment: %s (%s -> %s) and %s (%s -> %s)." %
                                     (name1, result_qualitative, name2, name1, player1.rating,
@@ -307,7 +307,7 @@ class Leaderboard:
         players = self.db.query(Player).filter(Player.rating != -1) \
             .order_by(Player.rating.desc()).limit(limit)
         for player in players:
-            ratings[player.jid] = {'name': sleekxmpp.jid.JID(player.jid).local,
+            ratings[player.jid] = {'name': JID(player.jid).local,
                                    'rating': player.rating}
         return ratings
 
@@ -326,13 +326,13 @@ class Leaderboard:
         """
         ratings = {}
         if nicks:
-            nicks = {sleekxmpp.jid.JID(jid, resource='0ad'): nick for jid, nick in nicks.items()}
+            nicks = {JID(jid).bare + "/0ad": nick for jid, nick in nicks.items()}
             player_filter = func.lower(Player.jid).in_([str(jid).lower() for jid in list(nicks)])
             players = self.db.query(Player.jid, Player.rating).filter(player_filter)
             for player in players:
                 rating = str(player.rating) if player.rating != -1 else ''
                 for jid in list(nicks):
-                    if jid == sleekxmpp.jid.JID(player.jid):
+                    if jid == JID(player.jid):
                         ratings[nicks[str(jid)]] = {'name': nicks[jid], 'rating': rating}
                         break
         return ratings
@@ -358,8 +358,7 @@ class ReportManager:
         """Add a game to the interface between a raw report and the leaderboard database.
 
         Arguments:
-            jid (sleekxmpp.jid.JID): JID of the player who submitted
-                the report
+            jid (JID): JID of the player who submitted the report
             raw_game_report (dict): Game report generated by 0ad
 
         """
@@ -369,7 +368,7 @@ class ReportManager:
         if match_id not in self.interim_report_tracker:
             self.interim_report_tracker[match_id] = {
                 'report': raw_game_report,
-                'jids': {player_index: str(sleekxmpp.jid.JID(jid, resource="0ad"))}
+                'jids': {player_index: str(JID(jid).bare + "/0ad")}
             }
         else:
             current_match = self.interim_report_tracker[match_id]
@@ -390,7 +389,7 @@ class ReportManager:
                                     player_jids[player_index], jid)
                 return
             else:
-                player_jids[player_index] = str(sleekxmpp.jid.JID(jid, resource="0ad"))
+                player_jids[player_index] = str(JID(jid).bare + "/0ad")
 
             num_players = self._get_num_players(raw_game_report)
             num_retrieved_reports = len(player_jids)
@@ -469,15 +468,24 @@ class ReportManager:
         return '\n'.join(difflib.ndiff(report1_list, report2_list))
 
 
-class EcheLOn(sleekxmpp.ClientXMPP):
+class EcheLOn(ClientXMPP):
     """Main class which handles IQ data and sends new data."""
 
     def __init__(self, sjid, password, room, nick, leaderboard):
-        """Initialize EcheLOn."""
-        sleekxmpp.ClientXMPP.__init__(self, sjid, password)
+        """Initialize EcheLOn.
+
+        Arguments:
+             sjid (JID): JID to use for authentication
+             password (str): password to use for authentication
+             room (JID): XMPP MUC room to join
+             nick (str): Nick to use in MUC
+             leaderboard (Leaderboard): Leaderboard instance to use
+
+        """
+        super().__init__(sjid, password)
         self.whitespace_keepalive = False
 
-        self.sjid = sleekxmpp.jid.JID(sjid)
+        self.sjid = JID(sjid)
         self.room = room
         self.nick = nick
 
@@ -489,25 +497,26 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         register_stanza_plugin(Iq, ProfileXmppPlugin)
 
         self.register_handler(Callback('Iq Boardlist', StanzaPath('iq@type=get/boardlist'),
-                                       self._iq_board_list_handler))
+                              self._iq_board_list_handler))
         self.register_handler(Callback('Iq GameReport', StanzaPath('iq@type=set/gamereport'),
-                                       self._iq_game_report_handler))
+                              self._iq_game_report_handler))
         self.register_handler(Callback('Iq Profile', StanzaPath('iq@type=get/profile'),
-                                       self._iq_profile_handler))
+                              self._iq_profile_handler))
 
         self.add_event_handler('session_start', self._session_start)
         self.add_event_handler('muc::%s::got_online' % self.room, self._muc_online)
         self.add_event_handler('muc::%s::got_offline' % self.room, self._muc_offline)
         self.add_event_handler('groupchat_message', self._muc_message)
+        self.add_event_handler('disconnected', lambda _: self.connect())
 
-    def _session_start(self, event):  # pylint: disable=unused-argument
+    async def _session_start(self, event):  # pylint: disable=unused-argument
         """Join MUC channel and announce presence.
 
         Arguments:
             event (dict): empty dummy dict
 
         """
-        self.plugin['xep_0045'].joinMUC(self.room, self.nick)
+        await self.plugin['xep_0045'].join_muc_wait(self.room, self.nick)
         self.send_presence()
         self.get_roster()
         logging.info("EcheLOn started")
@@ -516,17 +525,19 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         """Add joining players to the list of players.
 
         Arguments:
-            presence (sleekxmpp.stanza.presence.Presence): Received
+            presence (slixmpp.stanza.presence.Presence): Received
                 presence stanza.
 
         """
         nick = str(presence['muc']['nick'])
-        jid = sleekxmpp.jid.JID(presence['muc']['jid'])
+        jid = JID(presence['muc']['jid'])
 
         if not jid.resource.startswith('0ad'):
             return
 
-        self.leaderboard.get_or_create_player(sleekxmpp.jid.JID(jid, resource="0ad"))
+        jid_0ad_res = JID(jid)
+        jid_0ad_res.resource = "0ad"
+        self.leaderboard.get_or_create_player(jid_0ad_res)
 
         self._broadcast_rating_list()
 
@@ -536,12 +547,12 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         """Remove leaving players from the list of players.
 
         Arguments:
-            presence (sleekxmpp.stanza.presence.Presence): Received
+            presence (slixmpp.stanza.presence.Presence): Received
                 presence stanza.
 
         """
         nick = str(presence['muc']['nick'])
-        jid = sleekxmpp.jid.JID(presence['muc']['jid'])
+        jid = JID(presence['muc']['jid'])
 
         if not jid.resource.startswith('0ad'):
             return
@@ -555,7 +566,7 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         informative message.
 
         Arguments:
-            msg (sleekxmpp.stanza.message.Message): Received MUC
+            msg (slixmpp.stanza.message.Message): Received MUC
                 message
         """
         if msg['mucnick'] != self.nick and self.nick.lower() in msg['body'].lower():
@@ -569,14 +580,16 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         """Handle incoming leaderboard list requests.
 
         Arguments:
-            iq (sleekxmpp.stanza.iq.IQ): Received IQ stanza
+            iq (IQ): Received IQ stanza
 
         """
         if not iq['from'].resource.startswith('0ad'):
             return
 
         command = iq['boardlist']['command']
-        self.leaderboard.get_or_create_player(sleekxmpp.jid.JID(iq['from'], resource="0ad"))
+        jid = JID(iq['from'])
+        jid.resource = "0ad"
+        self.leaderboard.get_or_create_player(jid)
         if command == 'getleaderboard':
             try:
                 self._send_leaderboard(iq)
@@ -593,7 +606,7 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         """Handle end of game reports from clients.
 
         Arguments:
-            iq (sleekxmpp.stanza.iq.IQ): Received IQ stanza
+            iq (IQ): Received IQ stanza
 
         """
         if not iq['from'].resource.startswith('0ad'):
@@ -615,7 +628,7 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         """Handle profile requests from clients.
 
         Arguments:
-            iq (sleekxmpp.stanza.iq.IQ): Received IQ stanza
+            iq (IQ): Received IQ stanza
 
         """
         if not iq['from'].resource.startswith('0ad'):
@@ -631,12 +644,12 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         """Send the whole leaderboard.
 
         Arguments:
-            iq (sleekxmpp.stanza.iq.IQ): IQ stanza to reply to
+            iq (IQ): IQ stanza to reply to
 
         """
         ratings = self.leaderboard.get_board()
 
-        iq = iq.reply(clear=True)
+        iq = iq.reply()
         stanza = BoardListXmppPlugin()
         stanza.add_command('boardlist')
         for player in ratings.values():
@@ -644,7 +657,7 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         iq.set_payload(stanza)
 
         try:
-            iq.send(block=False)
+            iq.send()
         except Exception:
             logging.exception("Failed to send leaderboard to %s", iq['to'])
 
@@ -652,13 +665,12 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         """Send the ratings of all online players.
 
         Arguments:
-            iq (sleekxmpp.stanza.iq.IQ): IQ stanza to reply to
+            iq (IQ): IQ stanza to reply to
 
         """
         nicks = {}
-        for nick in self.plugin['xep_0045'].getRoster(self.room):
-            jid_str = self.plugin['xep_0045'].getJidProperty(self.room, nick, 'jid')
-            jid = sleekxmpp.jid.JID(jid_str)
+        for nick in self.plugin['xep_0045'].get_roster(self.room):
+            jid = JID(self.plugin['xep_0045'].get_jid_property(self.room, nick, 'jid'))
 
             if not jid.resource.startswith('0ad'):
                 continue
@@ -666,7 +678,7 @@ class EcheLOn(sleekxmpp.ClientXMPP):
             nicks[jid] = nick
         ratings = self.leaderboard.get_rating_list(nicks)
 
-        iq = iq.reply(clear=True)
+        iq = iq.reply()
         stanza = BoardListXmppPlugin()
         stanza.add_command('ratinglist')
         for player in ratings.values():
@@ -674,16 +686,15 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         iq.set_payload(stanza)
 
         try:
-            iq.send(block=False)
+            iq.send()
         except Exception:
             logging.exception("Failed to send rating list to %s", iq['to'])
 
     def _broadcast_rating_list(self):
         """Broadcast the ratings of all online players."""
         nicks = {}
-        for nick in self.plugin['xep_0045'].getRoster(self.room):
-            jid_str = self.plugin['xep_0045'].getJidProperty(self.room, nick, 'jid')
-            jid = sleekxmpp.jid.JID(jid_str)
+        for nick in self.plugin['xep_0045'].get_roster(self.room):
+            jid = JID(self.plugin['xep_0045'].get_jid_property(self.room, nick, 'jid'))
 
             if not jid.resource.startswith('0ad'):
                 continue
@@ -700,7 +711,7 @@ class EcheLOn(sleekxmpp.ClientXMPP):
             iq = self.make_iq_result(ito=jid)
             iq.set_payload(stanza)
             try:
-                iq.send(block=False)
+                iq.send()
             except Exception:
                 logging.exception("Failed to send rating list to %s", jid)
 
@@ -708,18 +719,22 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         """Send the player profile to a specified target.
 
         Arguments:
-            iq (sleekxmpp.stanza.iq.IQ): IQ stanza to reply to
+            iq (IQ): IQ stanza to reply to
             player_nick (str): The nick of the player to get the
                 profile for
 
         """
-        jid_str = self.plugin['xep_0045'].getJidProperty(self.room, player_nick, 'jid')
-        player_jid = sleekxmpp.jid.JID(jid_str, resource="0ad") if jid_str else None
+        jid_str = self.plugin['xep_0045'].get_jid_property(self.room, player_nick, 'jid')
+        if jid_str:
+            player_jid = JID(jid_str)
+            player_jid.resource = "0ad"
+        else:
+            player_jid = None
 
         # The player the profile got requested for is not online, so
         # let's assume the JID contains the nick as local part.
         if not player_jid:
-            player_jid = sleekxmpp.jid.JID('%s@%s/%s' % (player_nick, self.sjid.domain, '0ad'))
+            player_jid = JID('%s@%s/%s' % (player_nick, self.sjid.domain, '0ad'))
 
         try:
             stats = self.leaderboard.get_profile(player_jid)
@@ -727,7 +742,7 @@ class EcheLOn(sleekxmpp.ClientXMPP):
             logging.exception("Failed to get leaderboard profile for player %s", player_jid)
             stats = {}
 
-        iq = iq.reply(clear=True)
+        iq = iq.reply()
         stanza = ProfileXmppPlugin()
         if stats:
             stanza.add_item(player_nick, stats['rating'], stats['highestRating'],
@@ -739,7 +754,7 @@ class EcheLOn(sleekxmpp.ClientXMPP):
         iq.set_payload(stanza)
 
         try:
-            iq.send(block=False)
+            iq.send()
         except Exception:
             logging.exception("Failed to send profile to %s", iq['to'])
 
@@ -792,18 +807,20 @@ def main():
                         datefmt='%Y-%m-%d %H:%M:%S')
 
     leaderboard = Leaderboard(args.database_url)
-    xmpp = EcheLOn(sleekxmpp.jid.JID('%s@%s/%s' % (args.login, args.domain, 'CC')), args.password,
-                   args.room + '@conference.' + args.domain, args.nickname, leaderboard)
+    xmpp = EcheLOn(JID('%s@%s/%s' % (args.login, args.domain, 'CC')), args.password,
+                   JID(args.room + '@conference.' + args.domain), args.nickname, leaderboard)
     xmpp.register_plugin('xep_0030')  # Service Discovery
     xmpp.register_plugin('xep_0004')  # Data Forms
     xmpp.register_plugin('xep_0045')  # Multi-User Chat
     xmpp.register_plugin('xep_0060')  # Publish-Subscribe
     xmpp.register_plugin('xep_0199', {'keepalive': True})  # XMPP Ping
 
-    if xmpp.connect((args.xserver, 5222) if args.xserver else None, True, not args.xdisabletls):
-        xmpp.process()
+    if args.xserver:
+        xmpp.connect((args.xserver, 5222))
     else:
-        logging.error("Unable to connect")
+        xmpp.connect(None, True, not args.xdisabletls)
+
+    asyncio.get_event_loop().run_forever()
 
 
 if __name__ == '__main__':

xpartamupp/xpartamupp.py
@@ -18,16 +18,17 @@
 """0ad XMPP-bot responsible for managing game listings."""
 
 import argparse
+import asyncio
 import logging
 import sys
 import time
 
-import sleekxmpp
-
-from sleekxmpp.stanza import Iq
-from sleekxmpp.xmlstream.handler import Callback
-from sleekxmpp.xmlstream.matcher import StanzaPath
-from sleekxmpp.xmlstream.stanzabase import register_stanza_plugin
+from slixmpp import ClientXMPP
+from slixmpp.jid import JID
+from slixmpp.stanza import Iq
+from slixmpp.xmlstream.handler import Callback
+from slixmpp.xmlstream.matcher import StanzaPath
+from slixmpp.xmlstream.stanzabase import register_stanza_plugin
 
 from xpartamupp.stanzas import GameListXmppPlugin
 from xpartamupp.utils import LimitedSizeDict
@@ -44,8 +45,7 @@ class Games:
         """Add a game.
 
         Arguments:
-            jid (sleekxmpp.jid.JID): JID of the player who started the
-                game
+            jid (JID): JID of the player who started the game
             data (dict): information about the game
 
         Returns:
@@ -67,8 +67,7 @@ class Games:
         """Remove a game attached to a JID.
 
         Arguments:
-            jid (sleekxmpp.jid.JID): JID of the player whose game to
-                remove.
+            jid (JID): JID of the player whose game to remove.
 
         Returns:
             True if removing the game succeeded, False if not
@@ -96,8 +95,7 @@ class Games:
         """Switch game state between running and waiting.
 
         Arguments:
-            jid (sleekxmpp.jid.JID): JID of the player whose game to
-                change
+            jid (JID): JID of the player whose game to change
             data (dict): information about the game
 
         Returns:
@@ -128,20 +126,20 @@ class Games:
             return True
 
 
-class XpartaMuPP(sleekxmpp.ClientXMPP):
+class XpartaMuPP(ClientXMPP):
     """Main class which handles IQ data and sends new data."""
 
     def __init__(self, sjid, password, room, nick):
         """Initialize XpartaMuPP.
 
         Arguments:
-             sjid (sleekxmpp.jid.JID): JID to use for authentication
+             sjid (JID): JID to use for authentication
              password (str): password to use for authentication
-             room (str): XMPP MUC room to join
+             room (JID): XMPP MUC room to join
              nick (str): Nick to use in MUC
 
         """
-        sleekxmpp.ClientXMPP.__init__(self, sjid, password)
+        super().__init__(sjid, password)
         self.whitespace_keepalive = False
 
         self.room = room
@@ -158,15 +156,16 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
         self.add_event_handler('muc::%s::got_online' % self.room, self._muc_online)
         self.add_event_handler('muc::%s::got_offline' % self.room, self._muc_offline)
         self.add_event_handler('groupchat_message', self._muc_message)
+        self.add_event_handler('disconnected', lambda _: self.connect())
 
-    def _session_start(self, event):  # pylint: disable=unused-argument
+    async def _session_start(self, event):  # pylint: disable=unused-argument
         """Join MUC channel and announce presence.
 
         Arguments:
             event (dict): empty dummy dict
 
         """
-        self.plugin['xep_0045'].joinMUC(self.room, self.nick)
+        await self.plugin['xep_0045'].join_muc_wait(self.room, self.nick)
         self.send_presence()
         self.get_roster()
         logging.info("XpartaMuPP started")
@@ -178,12 +177,12 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
         are currently there.
 
         Arguments:
-            presence (sleekxmpp.stanza.presence.Presence): Received
+            presence (slixmpp.stanza.presence.Presence): Received
                 presence stanza.
 
         """
         nick = str(presence['muc']['nick'])
-        jid = sleekxmpp.jid.JID(presence['muc']['jid'])
+        jid = JID(presence['muc']['jid'])
 
         if not jid.resource.startswith('0ad'):
             return
@@ -199,12 +198,12 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
         don't end up with stale games.
 
         Arguments:
-            presence (sleekxmpp.stanza.presence.Presence): Received
+            presence (slixmpp.stanza.presence.Presence): Received
                 presence stanza.
 
         """
         nick = str(presence['muc']['nick'])
-        jid = sleekxmpp.jid.JID(presence['muc']['jid'])
+        jid = JID(presence['muc']['jid'])
 
         if not jid.resource.startswith('0ad'):
             return
@@ -221,7 +220,7 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
         informative message.
 
         Arguments:
-            msg (sleekxmpp.stanza.message.Message): Received MUC
+            msg (slixmpp.stanza.message.Message): Received MUC
                 message
         """
         if msg['mucnick'] != self.nick and self.nick.lower() in msg['body'].lower():
@@ -235,7 +234,7 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
         """Handle game state change requests.
 
         Arguments:
-            iq (sleekxmpp.stanza.iq.IQ): Received IQ stanza
+            iq (IQ): Received IQ stanza
 
         """
         if not iq['from'].resource.startswith('0ad'):
@@ -253,7 +252,7 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
         else:
             logging.info('Received unknown game command: "%s"', command)
 
-        iq.reply(clear=not success)
+        iq = iq.reply(clear=not success)
         if not success:
             iq['error']['condition'] = "undefined-condition"
         iq.send()
@@ -271,7 +270,7 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
         clients.
 
         Arguments:
-            to (sleekxmpp.jid.JID): Player to send the game list to.
+            to (JID): Player to send the game list to.
                 If None, the game list will be broadcasted
         """
         games = self.games.get_all_games()
@@ -281,9 +280,8 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
             stanza.add_game(games[jid])
 
         if not to:
-            for nick in self.plugin['xep_0045'].getRoster(self.room):
-                jid_str = self.plugin['xep_0045'].getJidProperty(self.room, nick, 'jid')
-                jid = sleekxmpp.jid.JID(jid_str)
+            for nick in self.plugin['xep_0045'].get_roster(self.room):
+                jid = JID(self.plugin['xep_0045'].get_jid_property(self.room, nick, 'jid'))
 
                 if not jid.resource.startswith('0ad'):
                     continue
@@ -291,14 +289,14 @@ class XpartaMuPP(sleekxmpp.ClientXMPP):
                 iq = self.make_iq_result(ito=jid)
                 iq.set_payload(stanza)
                 try:
-                    iq.send(block=False)
+                    iq.send()
                 except Exception:
                     logging.exception("Failed to send game list to %s", jid)
         else:
             iq = self.make_iq_result(ito=to)
             iq.set_payload(stanza)
             try:
-                iq.send(block=False)
+                iq.send()
             except Exception:
                 logging.exception("Failed to send game list to %s", to)
 
@@ -348,18 +346,20 @@ def main():
                         format='%(asctime)s %(levelname)-8s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    xmpp = XpartaMuPP(sleekxmpp.jid.JID('%s@%s/%s' % (args.login, args.domain, 'CC')),
-                      args.password, args.room + '@conference.' + args.domain, args.nickname)
+    xmpp = XpartaMuPP(JID('%s@%s/%s' % (args.login, args.domain, 'CC')), args.password,
+                      JID(args.room + '@conference.' + args.domain), args.nickname)
     xmpp.register_plugin('xep_0030')  # Service Discovery
     xmpp.register_plugin('xep_0004')  # Data Forms
     xmpp.register_plugin('xep_0045')  # Multi-User Chat
     xmpp.register_plugin('xep_0060')  # Publish-Subscribe
     xmpp.register_plugin('xep_0199', {'keepalive': True})  # XMPP Ping
 
-    if xmpp.connect((args.xserver, 5222) if args.xserver else None, True, not args.xdisabletls):
-        xmpp.process()
+    if args.xserver:
+        xmpp.connect((args.xserver, 5222))
     else:
-        logging.error("Unable to connect")
+        xmpp.connect(None, True, not args.xdisabletls)
+
+    asyncio.get_event_loop().run_forever()
 
 
 if __name__ == '__main__':

commit fc26e5adfa1083aa2ad5da03dc872cb1d673ae47

Fix unencrypted connections to XMPP server
219ce9f broke unencrypted connections to the XMPP server when using the
`--disable-tls` flag, as SleekXMPP and Slixmpp require different
parameters for `ClientXMPP.connect()`. This commit fixes that again.

xpartamupp/echelon.py
@@ -860,9 +860,9 @@ def main():
     xmpp.register_plugin('xep_0199', {'keepalive': True})  # XMPP Ping
 
     if args.xserver:
-        xmpp.connect((args.xserver, 5222))
+        xmpp.connect((args.xserver, 5222), disable_starttls=args.xdisabletls)
     else:
-        xmpp.connect(None, True, not args.xdisabletls)
+        xmpp.connect(None, disable_starttls=args.xdisabletls)
 
     asyncio.get_event_loop().run_until_complete(xmpp.shutdown)
 

xpartamupp/xpartamupp.py
@@ -400,9 +400,9 @@ def main():
     xmpp.register_plugin('xep_0199', {'keepalive': True})  # XMPP Ping
 
     if args.xserver:
-        xmpp.connect((args.xserver, 5222))
+        xmpp.connect((args.xserver, 5222), disable_starttls=args.xdisabletls)
     else:
-        xmpp.connect(None, True, not args.xdisabletls)
+        xmpp.connect(None, disable_starttls=args.xdisabletls)
 
     asyncio.get_event_loop().run_until_complete(xmpp.shutdown)
 

tests/test_echelon.py
@@ -204,6 +204,6 @@ class TestMain(TestCase):
                                                           call('xep_0045'), call('xep_0060'),
                                                           call('xep_0199', {'keepalive': True})],
                                                          any_order=True)
-            xmpp_mock().connect.assert_called_once_with(None, True, True)
+            xmpp_mock().connect.assert_called_once_with(None, disable_starttls=False)
             asyncio_mock.get_event_loop.assert_called_once_with()
             asyncio_mock.get_event_loop.return_value.run_forever_assert_called_once_with()

tests/test_xpartamupp.py
@@ -162,6 +162,6 @@ class TestMain(TestCase):
                                                           call('xep_0045'), call('xep_0060'),
                                                           call('xep_0199', {'keepalive': True})],
                                                          any_order=True)
-            xmpp_mock().connect.assert_called_once_with(None, True, True)
+            xmpp_mock().connect.assert_called_once_with(None, disable_starttls=False)
             asyncio_mock.get_event_loop.assert_called_once_with()
             asyncio_mock.get_event_loop.return_value.run_forever_assert_called_once_with()